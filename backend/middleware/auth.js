656a98065af3cc811d5199a5437b8d3da8b6ecd0
Sathira-Wijeratne <swije27@gmail.com>
2025-06-27 20:12:19 +0530

    Refactor authentication middleware: remove unnecessary comments for clarity

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,9 +2,9 @@ const jwt = require('jsonwebtoken');
 const secretKey = process.env.JSON_WEB_TOKEN_SECRET_KEY;
 
 // method to verify the token received by the client
-const authenticateToken = (req, res, next) =>{ //what is 'next'?
-    const authHeader = req.headers['authorization']; // what is this header?
-    const token = authHeader && authHeader.split(' ')[1]; // what is happening in this line?
+const authenticateToken = (req, res, next) =>{
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
 
     if (token == null) {
         return res.sendStatus(401);
